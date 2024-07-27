9d6daa0ea322e905f9050cc67d662e1bb914337a
Mohammed Gad <63354836+MohammedGad23@users.noreply.github.com>
2024-07-27 14:43:09 +0300

    Update script_.js

--- script_.js
+++ script_.js
@@ -1,28 +1,4 @@
-// Q1 swap Ans
-
-let a=10, b = 600;
-console.log("a = "+ a,"  b = " + b);
-[a,b] = [b,a];
-
-console.log("a = "+ a,"  b = " + b);
-
-// Q2 min and max
-
-var arr = [1,2,3,4,5,6,7,8,9,10]
-function minMAx(...arr1){
-    let ans = {"min" :Math.min(...arr1), "max":Math.max(...arr1)}
-    return ans;
-}
-let ans = minMAx(...arr);
-
-console.log("min is " + ans.min);
-console.log("max is " +ans.max);
-
-// Answer Day 2 Qa
-
 var result  = fetch("https://jsonplaceholder.typicode.com/users");
-
-
 result.then((e)=>{
  return  e.json()
 })
@@ -48,9 +24,6 @@ function click__(){
         getUsers(idx);
     })
 }
-
-// Answer Day 2 Qb
-
 async function getUsers(id){
     try{
     var result  = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
